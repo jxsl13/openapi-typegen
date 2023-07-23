eb44ae2ba8b742301c28318dff2bc7aa5b5d2332
jxsl13 <jxsl13@gmail.com>
2023-07-23 18:45:39 +0200

    ignore staticcheck strings.Title being deprecated

--- names/names.go
+++ names/names.go
@@ -35,6 +35,7 @@ func ToTypeName(name string) string {
 }
 
 func ToTitle(name string) string {
+	//lint:ignore SA1019 for our use case this function is enough, as we only work with alphanumeric characters
 	return strings.Title(name)
 }
 
